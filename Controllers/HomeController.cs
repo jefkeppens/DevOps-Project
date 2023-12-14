06bd49982de3411fd09bbcb79cfbfb131e10d7ba
jefkeppens <r0885867@student.thomasmore.be>
2023-12-14 18:35:47 +0100

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using project.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Contentful.Core;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IContentfulClient _client;
+
+        public HomeController(ILogger<HomeController> logger, IContentfulClient client)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var parts = await _client.GetEntries<ComputerPart>();
+            return View(parts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ComputerPart.cs
@@ -0,0 +1,17 @@
+﻿using Contentful.Core.Models;
+
+namespace project.Models
+{
+    public class ComputerPart
+    {
+        public string Name { get; set; }
+
+        public string Part { get; set; }
+
+        public string Company { get; set; }
+
+        public int Price { get; set; }
+
+        public Asset Image {  get; set; }
+    }
+}
